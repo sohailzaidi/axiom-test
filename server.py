4d48407b3fb43088a070ac281c33fd3c534788cc
Sohail Zaidi <s.m.sohailzaidi91@gmail.com>
2019-04-04 18:15:01 +0500

    commit testing

--- server.py
+++ server.py
@@ -1,1 +1,9 @@
-print ("Hello,  git!")
+weight = int(input("what is you weight in kg? "))
+age = int(input("what is your age? "))
+height = int(input("what is your height in inches? "))
+time = int(input("what was your running time? "))
+if weight < 60 and age > 17 and height > 50 and time < 50:
+    
+    print("Welcome to the Fatimiyah College sports team")
+else:
+    print("Sorry you are not eligible for team")
